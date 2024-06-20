9e58c23c789282f3303410cdc066a1a675233904
njanfang <njanfang@gmail.com>
2024-06-20 16:31:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();app.get('/', function (req, res) {
   res.send('Hello World!');
 });app.listen(3000, function () {
-  console.log('Example app listening on port 2000!');
+  console.log('Example app listening on port 1000!');
 });
